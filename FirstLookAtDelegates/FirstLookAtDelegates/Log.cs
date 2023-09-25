b5e1b3d40f9746a2ee567f5c2b347c5451d5a002
Aleksander Oleszkiewicz <aleksander.oleszkiewicz0@gmail.com>
2023-09-25 21:22:22 +0200

    First look at the delegates

---
+++ FirstLookAtDelegates/FirstLookAtDelegates/Log.cs
@@ -0,0 +1,15 @@
+﻿namespace FirstLookAtDelegates;
+
+public class Log
+{
+    public void LogTextToConsole(string text)
+    {
+        Console.WriteLine($"Date: {DateTime.Now} - {text}");
+    }
+
+    public void LogTextToFile(string text)
+    {
+        using StreamWriter streamWriter = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "log.txt"), true);
+        streamWriter.WriteLine($"Date: {DateTime.Now} - {text}");
+    }
+}
---
+++ FirstLookAtDelegates/FirstLookAtDelegates/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace FirstLookAtDelegates;
+
+
+public static class Program
+{
+    private delegate void LogDelegate(string text);
+    
+    public static void Main(string[] args)
+    {
+        var log = new Log();
+
+        var logTextToConsoleDel = new LogDelegate(log.LogTextToConsole);
+        var logTextToFileDel = new LogDelegate(log.LogTextToFile);
+        
+        var multiLogDel = logTextToConsoleDel + logTextToFileDel;
+        
+        Console.WriteLine("Please enter your name:");
+        var name = Console.ReadLine();
+
+        if (name != null) LogText(multiLogDel, name);
+    }
+
+    private static void LogText(LogDelegate logDelegate, string text)
+    {
+        logDelegate(text);
+    }
+}
