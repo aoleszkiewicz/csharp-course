8ebae8976f05642247744ddea339f09227fe5936
Aleksander Oleszkiewicz <aleksander.oleszkiewicz0@gmail.com>
2023-09-25 18:25:03 +0200

    static modifier

---
+++ Static_Modifier/Static_Modifier/Book.cs
@@ -0,0 +1,23 @@
+﻿namespace Static_Modifier;
+
+public class Book
+{
+    private string _title;
+    private string _author;
+    private int _pages;
+    private static int _bookCount;
+    
+    public Book(string title, string author, int pages)
+    {
+        _title = title;
+        _author = author;
+        _pages = pages;
+        // Increment _bookCount everytime book object is created.
+        _bookCount++;
+    }
+
+    public static int GetBookCount()
+    {
+        return _bookCount;
+    }
+}
---
+++ Static_Modifier/Static_Modifier/Program.cs
@@ -0,0 +1,13 @@
+﻿using Static_Modifier;
+
+public static class Program
+{
+    public static void Main()
+    {
+        Book book1 = new Book("Harry Potter", "JK Rowling", 400);
+        Book book2 = new Book("Harry Potter", "JK Rowling", 400);
+        Book book3 = new Book("Harry Potter", "JK Rowling", 400);
+        
+        System.Console.WriteLine(Book.GetBookCount());
+    }
+}
